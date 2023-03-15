3c828159d651119db5d6978a4025f6d49e7c1d9d
vincenttommi <vincenttommi@gmail.com>
2023-03-15 22:59:11 +0300

    inline functions

---
+++ index.js
@@ -0,0 +1,102 @@
+// // function monday(){
+
+
+// // console.log("Go for  a five-mile run");
+// // console.log("Pump iron");
+
+// // }
+
+// // function Tuesday(){
+
+// // console.log("Go for a five-mile run");
+// // console.log("Swim  40  laps");
+
+// // }
+
+
+// // function wednesday(){
+
+// // console.log("Go for a five-mile run");
+// // console.log("Go for a five mile run");
+
+
+// // }    
+
+
+
+// // function Thursday(){
+
+// // console.log("Go for  a five-mile run");
+// // console.log("pump iron");
+
+
+
+// // }
+
+
+// // function friday(){
+
+// // console.log("Go for a five-mile run");
+// // console.log("Swim 40 laps");
+
+// // }
+
+
+
+// function  runFiveMiles(){
+
+// console.log("Go for  a five-mile run");
+
+
+// }
+
+
+// function  liftWeights(){
+
+// console.log("pump iron");
+
+
+// }
+
+
+// function swimFortyLaps(){
+
+// console.log("Swim 40 laps");
+
+// }
+
+
+// function monday(){
+
+//     runFiveMiles();
+//     liftWeights();
+// }
+
+// function exerciseRoutine(postRunActivity){
+
+// runFiveMiles();
+// postRunActivity();
+
+
+// }
+
+
+// function Monday() {
+//   exerciseRoutine(liftWeights);
+// }
+
+// function exerciseRoutine(postRunActivity) {
+//   runFiveMiles();
+//   postRunActivity();
+// }
+
+// console.log(monday());
+
+exerciseRoutine(() => {
+
+
+    console.log("Stretch!   work that core ");      
+
+
+});
+    
